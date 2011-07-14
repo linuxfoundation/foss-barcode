6a788c6dd2f380e94b53f170410120275ee88702
Stew Benedict <stewb@linuxfoundation.org>
2011-07-14 18:57:13 -0400

    Fix the "Select All" hide/show in the input tab

--- fossbarcode/media/js/visibility.js
+++ fossbarcode/media/js/visibility.js
@@ -1,7 +1,25 @@
 function toggle_visibility(id) {
     var e = document.getElementById(id);
-    if (e.style.display == 'block')
-        e.style.display = 'none';
-    else
+    if (typeof e != 'undefined') {
+        if (e.style.display == 'block')
+            e.style.display = 'none';
+        else
+            e.style.display = 'block';
+    }
+}
+function show_none(id) {
+    var e = document.getElementById(id);
+    if (typeof e != 'undefined')
+        e.style.display = '';
+}
+function show_block(id) {
+    var e = document.getElementById(id);
+    if (typeof e != 'undefined')
         e.style.display = 'block';
 }
+function hide_element(id) {
+    var e = document.getElementById(id);
+    if (typeof e != 'undefined')
+        e.style.display = 'none';
+}
+
