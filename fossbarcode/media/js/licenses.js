1782edc4594d32abe41f12dc2e9ce3016d5c080a
Jeff Licquia <jeff@licquia.org>
2011-09-14 22:30:11 -0400

    Get new license add code working on the input form.

--- fossbarcode/media/js/licenses.js
+++ fossbarcode/media/js/licenses.js
@@ -17,16 +17,23 @@ function populate_licenses(select_element_id) {
 }
 
 function select_license(select_element_id, license_id) {
-    $('#' + select_element_id).children().each(function() {
-        if (this.value == license_id) {
-            this.selected = "selected";
-        } else {
-            this.selected = "";
-        }
+    $.getJSON(barcode_licenses_url, function(data) {
+	var items = [];
+	$.each(data, function() {
+            var new_option = '<option value="'+ this[0] + '"';
+            if (Number(this[0]) == license_id) {
+                new_option = new_option + ' selected="selected"';
+            }
+	    items.push(new_option + '>' + this[1] + '</option>\n');
+	});
+	items.push('<option value="-1">Other License...</option>\n');
+	$('#' + select_element_id).html(items.join(''));
     });
 }
 
 function create_new_license(license_name, license_version, callback) {
-    return $.getJSON(barcode_new_license_url + "?license_name=" + license_name + "&license_version=" + license_version,
+    return $.getJSON(barcode_new_license_url + "?",
+		     { 'license_name': license_name,
+		       'license_version': license_version },
 		     callback);
 }
